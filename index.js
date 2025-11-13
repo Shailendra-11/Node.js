97b53fc23ff35d7995725a3d44d69d2a734720ef
Shailendra Rai <shailendrarai153@gmail.com>
2025-11-13 18:25:56 +0530

    start

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hii")
+const express = require("express")
+const app  = express()
+const fs = require("fs")
+const url = require("url")
+const { error } = require("console")
+
+// const myserver = http.createServer((req, res) => {
+//      const log = `${Date.now()} this server append file\n`
+//      fs.appendFile("logo.text", log, (error, data) => {
+//           res.end("Hello kdf")
+//      })
+// })
+
+app.get('/test' ,(req ,res)=>{
+   res.send("hii")
+})
+
+app.listen(300, () => {
+     console.log("server start")
+})
